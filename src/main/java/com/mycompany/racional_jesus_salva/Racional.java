2d656e1790581e070b04a2573bf0013e2ea94ea6
salva <salvi205@hotmail.com>
2019-12-11 13:21:51 +0100

    Constructor por defecto

--- src/main/java/com/mycompany/racional_jesus_salva/Racional.java
+++ src/main/java/com/mycompany/racional_jesus_salva/Racional.java
@@ -10,5 +10,17 @@ package com.mycompany.racional_jesus_salva;
  * @author salva
  */
 public class Racional {
-    
+
+    /*Un número Racional se representa por dos números enteros a/b, donde a es el numerador y b es el denominador, 
+    ambos números enteros. Los objetos de tipo Racional estarán encapsulados.
+     */
+
+    private int a;
+    private int b;
+//A. Constructor por defecto, indicando como numerador y denominador el valor 1.
+
+    public Racional() {
+        this.a = 1;
+        this.b = 1;
+    }
 }
