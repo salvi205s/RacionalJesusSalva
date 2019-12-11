3ed3735f859a01378ecd46092bdb5782ed6050db
salva <salvi205@hotmail.com>
2019-12-11 14:18:49 +0100

    Clase main, con dor numeros racionales creados, z y k

---
+++ src/main/java/com/mycompany/racional_jesus_salva/AppRacional.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.racional_jesus_salva;
+
+/**
+ *
+ * @author salva
+ */
+public class AppRacional {
+    
+    public static void main(String[] args) {
+                
+//Crea dos racionales  con valores z=4/5 y k=3/4.
+        Racional z=new Racional(4, 5);
+        Racional k=new Racional(3, 4);
+
+        
+        
+    }
+}
